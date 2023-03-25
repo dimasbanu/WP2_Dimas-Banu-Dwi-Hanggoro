d439323200fac8b937a7fb63c67570e0e24e844e
dimasbanu <dimasbanudh@gmail.com>
2023-03-25 22:01:08 +0700

    Update contoh1.php

--- pustaka-booking/controllers/contoh1.php
+++ pustaka-booking/controllers/contoh1.php
@@ -4,9 +4,8 @@ class Contoh1 extends CI_Controller
 	public function index()
 	{
 		echo "<h1>Perkenalkan</h1>";
-		echo"Nama saya Imam Nawawi
-			Saya tingga di daerah Ciputat
-			olah raga yang saya sukai adalah
-			Bulutangkis";
+		echo"Nama Dimas Banu Dwi Hanggoro
+			Saya tingga di daerah Pondok Kelapa.
+			Hobi saya nonton drama dan film.";
 	}
 }
